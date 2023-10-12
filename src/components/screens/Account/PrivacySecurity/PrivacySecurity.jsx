65b6d56f8c6f8430d333e4f5e1a8ddce19e2c84f
Michail Bezverkhov <bezverhov2000@gmail.com>
2023-10-12 22:08:02 +0300

    privacy & security "main-info" block

--- src/components/screens/Account/PrivacySecurity/PrivacySecurity.jsx
+++ src/components/screens/Account/PrivacySecurity/PrivacySecurity.jsx
@@ -18,6 +18,31 @@ const PrivacySecurity = () => {
                     </li>
                 </ul>
             </div>
+
+
+            <div className="privacy-security__bottom">
+                <form className="privacy-security__info-form info-form">
+                    <h3 className="info-form__title">Main info</h3>
+                    <ul className="info-form__list">
+                        <li className="info-form__avatar">
+                            <button className="info-form__choose-avatar">Upload your avatar</button>
+                        </li>
+                        <li className="info-form__item">
+                            <label htmlFor="FirstName">First name</label>
+                            <input id="FirstName" placeholder="Please enter" type="text" />
+                        </li>
+                        <li className="info-form__item">
+                            <label htmlFor="LastName">Last name</label>
+                            <input id="LastName" placeholder="Please enter" type="text" />
+                        </li>
+                        <li className="info-form__item">
+                            <label htmlFor="Birthday">Birthday</label>
+                            <input id="Birthday" placeholder="Please enter" type="date" />
+                        </li>
+                    </ul>
+                </form>
+            </div>
+
         </div>
     )
 };
